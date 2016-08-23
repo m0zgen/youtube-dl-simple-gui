3b7eb9e9d8925c95925c8820a7bae3423928f99c
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2016-08-23 22:57:51 +0600

    Add git ignore

--- add-to-git.sh
+++ add-to-git.sh
@@ -2,10 +2,10 @@
 # Upload this lib to git
 # Created by Yevgeniy Goncharov, http://sys-adm.in
 
-if [[ -z youtube-dl-simple-gui ]]; then
+if [[ -z $1 ]]; then
   echo "Please add commit comment!"
 else
   git add .
-  git commit -m "youtube-dl-simple-gui"
+  git commit -m "$1"
   git push origin master
 fi
