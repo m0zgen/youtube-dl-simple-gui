4b3a7d7f5b15e6f3bb3e1a9e9d666224e1fcfbc7
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2016-08-23 22:53:24 +0600

    youtube-dl-simple-gui

--- youtube-dl-terminal.sh
+++ youtube-dl-terminal.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Simple terminal script for youtube-dl usage
 # Created by Yevgeniy Goncharov, http://sys-adm.in
-#
+
+# ---------------------------------------------------------- VARIABLES #
+
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPTPATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
 read -p "Paste YouTube video link URL: " url
 
@@ -14,6 +18,23 @@ youtube-dl -F $url
 
 read -p "Select video quality: " quality
 
-until youtube-dl -cit -f $quality $url; do sleep 5; done
+download() {
+  until youtube-dl -cit -f $quality $url; do sleep 3; done
+}
 
-# youtube-dl -cit -f $quality -o "/path/to/folder/%(title)s.%(ext)s" $url
+read -p "You can change download folder. Change (y/n) " answer
+
+if [[ $answer =~ [yY](es)* ]]; then
+
+    read -p "Folder path: " fpach
+    if [[ -d $fpach ]]; then
+      cd $fpach
+      download
+      cd - > /dev/null
+    else
+      echo "Folder does not exist, please try another. Exit."
+    fi
+  else
+    echo "Video save to script folder..."
+    download
+fi
